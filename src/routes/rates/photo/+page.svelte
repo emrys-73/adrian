645694c4db8278f5cf0623c03be61d6ab094f44f
Adrian Montoya <montoya@astralta.com>
2024-11-15 19:40:10 +0100

    Add Photo Rates

---
+++ src/routes/rates/photo/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { browser } from '$app/environment';
+
+    const pdfUrl = '/photopack.pdf';
+    
+    if (browser) {
+        window.location.href = pdfUrl;
+    }
+</script>
+
+<p>Downloading photoshoot rates...</p>
